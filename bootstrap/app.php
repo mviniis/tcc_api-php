78a688e0c1f20960bcc121def31a13845790eee3
Matheus Vinicius <matheusv.16santos@gmail.com>
2025-02-02 23:40:34 -0300

    feat: Configuracao para retornar json em exceções na API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+	->withRouting(
+		web: __DIR__ . '/../routes/web.php',
+		api: __DIR__ . '/../routes/api.php',
+		apiPrefix: 'api',
+		commands: __DIR__ . '/../routes/console.php',
+		health: '/up',
+	)
+	->withMiddleware(function (Middleware $middleware) {
+		//
+	})
+	->withExceptions(function (Exceptions $exceptions) {
+		// RETORNA JSON QUANDO OS ERROS FOREM EXIBIDOS NA API
+		$exceptions->shouldRenderJsonWhen(function(Request $request, Throwable $exception) {
+			return $request->is('api/*');
+		});
+	})->create();
